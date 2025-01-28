1d6eb69020df248b6f898b2a65044f99de477596
MerGatto <kepplinger.harald@gmail.com>
2025-01-28 20:12:18 +0100

    Initial commit

---
+++ ToxicTitan.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using StationeersMods.Interface;
+
+namespace ToxicTitan
+{
+    [StationeersMod("ToxicTitan","ToxicTitan [StationeersMods]","0.2.4657.21547.1")]
+    class ToxicTitan : ModBehaviour
+    {
+        // private ConfigEntry<bool> configBool;
+        public override void OnLoaded(ContentHandler contentHandler)
+        {
+            Harmony harmony = new Harmony("ToxicTitan");
+            harmony.PatchAll();
+            UnityEngine.Debug.Log("ToxicTitan Loaded!");
+        }
+    }
+}
---
+++ WeatherManagerPatch.cs
@@ -0,0 +1,71 @@
+﻿using Assets.Scripts.Atmospherics;
+using Assets.Scripts;
+using Assets.Scripts.Objects;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System.Reflection;
+using Weather;
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using static Assets.Scripts.Util.Defines;
+
+namespace ToxicTitan
+{
+    [HarmonyPatch(typeof(WeatherManager), nameof(WeatherManager.StopCurrentWeatherEvent))]
+    [UsedImplicitly]
+    public static class WeatherManager_StopCurrentWeatherEvent_Patch
+    {
+        public static void Postfix()
+        {
+            Type type = typeof(PlanetaryAtmosphereSimulation);
+
+            FieldInfo info = type.GetField("_globalGasMix", BindingFlags.NonPublic | BindingFlags.Static);
+            var globalGasMix = (GlobalGasMix)info.GetValue(null);
+
+            // Clear LiquidClouds
+            info = type.GetField("_liquidClouds", BindingFlags.NonPublic | BindingFlags.Static);
+            var liquidClouds = (GlobalGasMix)info.GetValue(null);
+            globalGasMix.Add(liquidClouds, AtmosphereHelper.MatterState.All);
+            liquidClouds.ClearQuantities(AtmosphereHelper.MatterState.All);
+
+            // Clear IceClouds
+            info = type.GetField("_iceClouds", BindingFlags.NonPublic | BindingFlags.Static);
+            var iceClouds = (GlobalGasMix)info.GetValue(null);
+            globalGasMix.Add(iceClouds, AtmosphereHelper.MatterState.All);
+            iceClouds.ClearQuantities(AtmosphereHelper.MatterState.All);
+        }
+    }
+
+    [HarmonyPatch(typeof(WeatherManager), "HasWorldStartCooldownPastInDays")]
+    [UsedImplicitly]
+    public static class WeatherManager_HasWorldStartCooldownPastInDays_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            if (WorldManager.CurrentWorldName == "ToxicTitan")
+            {
+                __result = WorldManager.DaysPast > 1;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(WeatherManager), nameof(WeatherManager.GetNextWeatherEvent))]
+    [UsedImplicitly]
+    public static class WeatherManager_GetNextWeatherEvent_Patch
+    {
+        private static bool _firstStorm = true;
+
+        public static void Postfix(ref WeatherEvent __result)
+        {
+            if (WorldManager.CurrentWorldName == "ToxicTitan" && _firstStorm)
+            {
+                ConsoleWindow.Print("Setting First Storm to TitanToxicRain");
+                _firstStorm = false;
+                __result = DataCollection.Get<WeatherEvent>(UnityEngine.Animator.StringToHash("TitanToxicRain"));
+            }
+        }
+    }
+
+
+}
